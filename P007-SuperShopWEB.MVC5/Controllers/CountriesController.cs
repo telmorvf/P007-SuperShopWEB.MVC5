e471bf40c3082e20e724f24dfd63d96402fefa55
Telmo Fernandes <telmorf@live.com>
2024-06-13 19:33:42 +0100

    MVC31 Drop Down Countries - Repository, Controller, ViewModel, Views

---
+++ P007-SuperShopWEB.MVC5/Controllers/CountriesController.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using P007_SuperShopWEB.MVC5.Data.Entities;
+using P007_SuperShopWEB.MVC5.Data;
+using P007_SuperShopWEB.MVC5.Models;
+using P007_SuperShopWEB.MVC5.Data.Repositories;
+
+
+namespace P007_SuperShopWEB.MVC5.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CountriesController : Controller
+    {
+        private readonly ICountryRepository _countryRepository;
+
+        public CountriesController(
+            ICountryRepository countryRepository
+            )
+        {
+            _countryRepository = countryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_countryRepository.GetCountriesWithCities());
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _countryRepository.GetCountryWithCitiesAsync(id.Value);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        public async Task<IActionResult> AddCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _countryRepository.GetByIdAsync(id.Value);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            var model = new CityViewModel { CountryId = country.Id };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCity(CityViewModel model)
+        {
+            if (this.ModelState.IsValid)
+            {
+                await _countryRepository.AddCityAsync(model);
+                return RedirectToAction("Details", new { id = model.CountryId });
+            }
+
+            return this.View(model);
+        }
+
+        public async Task<IActionResult> EditCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _countryRepository.GetCityAsync(id.Value);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return View(city);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditCity(City city)
+        {
+            if (this.ModelState.IsValid)
+            {
+                var countryId = await _countryRepository.UpdateCityAsync(city);
+                if (countryId != 0)
+                {
+                    return this.RedirectToAction($"Details", new { id = countryId });
+                }
+            }
+
+            return this.View(city);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _countryRepository.CreateAsync(country);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception)
+                {
+                    //_flashMessage.Warning("This country already exist!", "Atention!");
+                }
+
+                return View(country);
+            }
+
+            return View(country);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _countryRepository.GetByIdAsync(id.Value);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return View(country);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Country country)
+        {
+            if (ModelState.IsValid)
+            {
+                await _countryRepository.UpdateAsync(country);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(country);
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var country = await _countryRepository.GetByIdAsync(id.Value);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            await _countryRepository.DeleteAsync(country);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> DeleteCity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var city = await _countryRepository.GetCityAsync(id.Value);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var countryId = await _countryRepository.DeleteCityAsync(city);
+            return this.RedirectToAction($"Details", new { id = countryId });
+        }
+
+
+    }
+}
--- P007-SuperShopWEB.MVC5/Data/Repositories/ICountryRepository.cs
+++ P007-SuperShopWEB.MVC5/Data/Repositories/ICountryRepository.cs
@@ -1,29 +1,32 @@
-﻿using P007_SuperShopWEB.MVC5.Data.Entities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using P007_SuperShopWEB.MVC5.Data.Entities;
+using P007_SuperShopWEB.MVC5.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 
 namespace P007_SuperShopWEB.MVC5.Data.Repositories
 {
-    public interface ICountryRepository
+    public interface ICountryRepository : IGenericRepository<Country>
     {
-        public interface ICountryRepository : IGenericRepository<Country>
-        {
-            //IQueryable GetCountriesWithCities();
+        IQueryable GetCountriesWithCities();
 
-            //Task<Country> GetCountryWithCitiesAsync(int id);
+        Task<Country> GetCountryWithCitiesAsync(int id);
 
-            //Task<City> GetCityAsync(int id);
+        Task<City> GetCityAsync(int id);
 
-            //Task AddCityAsync(CityViewModel model);
+        Task AddCityAsync(CityViewModel model);
 
-            //Task<int> UpdateCityAsync(City city);
+        Task<int> UpdateCityAsync(City city);
 
-            //Task<int> DeleteCityAsync(City city);
+        Task<int> DeleteCityAsync(City city);
 
-            //IEnumerable<SelectListItem> GetComboCountries();
+        //IEnumerable<SelectListItem> GetComboCountries();
 
-            //IEnumerable<SelectListItem> GetComboCities(int countryId);
+        //IEnumerable<SelectListItem> GetComboCities(int countryId);
 
-            //Task<Country> GetCountryAsync(City city);
+        //Task<Country> GetCountryAsync(City city);
 
-        }
     }
 }
